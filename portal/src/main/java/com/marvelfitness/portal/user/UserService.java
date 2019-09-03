86db3c5d438a71dd2b5ad0d88a19ef6cbe328c9d
Quintin Roper <quintinroper@gmail.com>
2019-09-03 17:22:46 -0600

    Add Email Functionality
    Create SMTP server and send email endpoint

--- portal/src/main/java/com/marvelfitness/portal/user/UserService.java
+++ portal/src/main/java/com/marvelfitness/portal/user/UserService.java
@@ -1,8 +1,12 @@
 package com.marvelfitness.portal.user;
 
+import com.marvelfitness.portal.rewards.Reward;
+import com.marvelfitness.portal.rewards.RewardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -14,6 +18,12 @@ public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Autowired
+    private RewardService rewardService;
 
     /**
      * Gets a list of all Users from the database
@@ -125,12 +135,37 @@ public class UserService {
      * Updates the rewards balance of the Customer
      * @param user_id id of Customer to update
      * @param new_balance new balance for Customer
+     * @return appropriate ResponseEntity (404 if invalid user_id, 200 if valid)
      */
     public ResponseEntity updateRewardsBalance(int user_id, short new_balance) {
         User customer = getCustomerById(user_id);
         if (customer != null) {
             customer.setRewards_balance(new_balance);
             userRepository.save(customer);
+            return new ResponseEntity(HttpStatus.OK);
+        }
+        return new ResponseEntity(HttpStatus.NOT_FOUND);
+    }
+
+    /**
+     * Emails the Customer to confirm reward redemption
+     * @param user_id id of Customer to email
+     * @param reward_id id of Reward redeemed
+     * @return appropriate ResponseEntity (404 if invalid user_id, 200 if valid)
+     */
+    public ResponseEntity sendEmail(int user_id, int reward_id) {
+        User user = getCustomerById(user_id);
+        if (user != null) {
+            Reward reward = rewardService.getReward(reward_id);
+            SimpleMailMessage msg = new SimpleMailMessage();
+            msg.setTo(user.getEmail());
+            msg.setSubject("You have earned " + reward.getDescription());
+            String message = "Dear " + user.getName() + ",\n\nCongratulations! You have earned " +
+                    reward.getDescription() + "\n\nPlease show this email at your next visit to receive " +
+                    "your reward!\n\nThank you,\nMarvel Fitness Management";
+            msg.setText(message);
+
+            javaMailSender.send(msg);
             return new ResponseEntity(HttpStatus.OK);
         }
         return new ResponseEntity(HttpStatus.NOT_FOUND);
