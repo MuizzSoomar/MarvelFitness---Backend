41bd273a9059ba1a48005f7869f1e7e89dbb7c2f
Quintin Roper <quintinroper@gmail.com>
2019-09-09 10:47:30 -0600

    Rewards javadocs
    Added javadocs to Rewards package

--- portal/src/main/java/com/marvelfitness/portal/rewards/RewardRepository.java
+++ portal/src/main/java/com/marvelfitness/portal/rewards/RewardRepository.java
@@ -2,6 +2,9 @@ package com.marvelfitness.portal.rewards;
 
 import org.springframework.data.repository.CrudRepository;
 
+/**
+ * Reward Repository interface used to access the reward in the database
+ */
 public interface RewardRepository extends CrudRepository<Reward, Integer> {
 
 }
